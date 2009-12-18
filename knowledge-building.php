48cdc464fdb5b10790baeac00a7eb69034741f82
tatti <tatti@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-18 09:49:19 +0000

    Tagged stable version 0.5.3. Tested with Wordpress 2.9 RC1. Added more instructions to readme.
    git-svn-id: https://plugins.svn.wordpress.org/knowledge-building/trunk@184267 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- knowledge-building.php
+++ knowledge-building.php
@@ -3,7 +3,7 @@
 Plugin Name: Knowledge Building
 Plugin URI: http://fle4.uiah.fi/kb-wp-plugin
 Description: Allows knowledge building processes in the comments of posts.
-Version: 0.5.2
+Version: 0.5.3
 Author: Tarmo Toikkanen
 Author URI: http://tarmo.fi
 */
