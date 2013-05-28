5a000761937833771e55b7f73f2c0474026d28f3
Spikefinned <Spikefinned@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-28 08:42:01 +0000

    Version 0.6.11
    git-svn-id: https://plugins.svn.wordpress.org/knowledge-building/tags/0.6.10@719262 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ map.php
@@ -0,0 +1,163 @@
+<?php
+$replies = get_comments(array(
+			'status' => 'approve',
+			'post_id' => get_the_ID(),
+			'orderby' => 'date'
+			));
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+	<title><?php the_title(); ?> (Map) | <?php bloginfo('name'); ?></title>
+	<link href='http://fonts.googleapis.com/css?family=Junge' rel='stylesheet' type='text/css'>
+	<?php wp_head(); ?>
+</head>
+<body class="knbu-map-view <?php echo isset($_GET['map-frame']) ? 'knbu-map-frame' : 'knbu-map-full'; ?>">
+	<div id="map">
+		<div id="raven"></div>
+		<div id="fps"></div>
+		<div id="grouping">
+			<ul>
+				<li><a id="grouping-byknowledgetypes">Group by knowledge types</a></li>
+				<li><a id="grouping-byauthors">Group by authors</a></li>
+				<li><a id="grouping-discussion">Discussion</a></li>
+				<li><a id="grouping-time">Time</a></li>
+			</ul>
+		</div>
+		<div id="navigation">
+			<div id="pan">
+				<img src="<?php echo plugins_url(); ?>/knowledge-building/images/navi-bg.png" id="navigation-background">
+				<img src="<?php echo plugins_url(); ?>/knowledge-building/images/arrow-left.png" class="arrow" id="arrow-left">
+				<img src="<?php echo plugins_url(); ?>/knowledge-building/images/arrow-up.png" class="arrow" id="arrow-up">
+				<img src="<?php echo plugins_url(); ?>/knowledge-building/images/arrow-right.png" class="arrow" id="arrow-right">
+				<img src="<?php echo plugins_url(); ?>/knowledge-building/images/arrow-down.png" class="arrow" id="arrow-down">
+				<img src="<?php echo plugins_url(); ?>/knowledge-building/images/center.png" class="arrow" id="arrow-center">
+				<!-- <div class="left"></div>
+				<div class="right"></div>
+				<div class="up"></div>
+				<div class="down"></div>
+				<div class="center"></div> -->
+			</div>
+			<div id="zoom"></div>
+		</div>
+		<div id="legend">
+			<ul>
+				<?php knbu_get_legends(); ?>
+			</ul>
+		</div>
+		<?php if(isset($_GET['map-frame'])) { ?>
+		<div id="full-screen">
+			<a href="<?php echo remove_query_arg( 'map-frame' ); ?>" target="_blank">
+				<img src="<?php echo plugins_url() ?>/knowledge-building/images/full-screen.png" id="full-screen">
+			</a>
+		</div>
+		<?php } ?>
+	</div>
+	
+	<div id="message"><?php knbu_comment(get_the_ID()); ?></div>
+		<?php
+			usort($replies, 'knbu_cmp');
+			knbu_get_childs(get_the_ID(), $replies);
+		?>
+	</body>
+</html>
+<?php
+
+
+function knbu_get_childs($id, $replies) {
+	global $knowledgeTypes, $knbu_kbsets, $post;
+	
+	// The post itself will be the starting node
+	$nodes = array(
+				array(	
+					'id' => 0,
+					'parent' => false,
+					'content' => $post->post_content,
+					'avatar' => knbu_get_avatar_url( $post->user_id ),
+					'username' => get_the_author_meta( 'display_name', $post->post_author ),
+					'email' => $post->user_email,
+					'date' => date(get_option('date_format').' '.get_option('time_format'), strtotime($post->post_date)),
+					'timestamp' => strtotime( $post->post_date ),
+					'typeName' => 'Start',
+					'title' => $post->post_title,
+					'static' => true,
+					'index' => 1
+				)
+	);
+	
+	
+	$map_index = get_post_meta( $id, 'knbu_map_index', true);
+	
+	if(empty($map_index))
+		$map_index = 2;
+		
+	foreach($replies as $reply) {
+		
+		// Get comment meta data
+		// Should be combined to one variable(?)
+		$type = get_comment_meta($reply->comment_ID, 'kbtype', true);
+		$title = get_comment_meta($reply->comment_ID, 'comment_title', true);
+		$node_index = get_comment_meta( $reply->comment_ID, 'knbu_map_comment_index' );
+		$anchor = json_decode(get_comment_meta($reply->comment_ID, 'node_position', true));
+		
+		// Default values
+		$name = 'Unspecified';
+		$color = '#000';
+		
+		// Get type name and color 
+		foreach($knbu_kbsets[knbu_get_kbset_for_post(get_the_ID())]->KnowledgeTypeSet->KnowledgeType as $t) {	
+			if($t['ID'] == $type) {
+				$name = (string)$t['Name']; 
+				$color = (string)$t['Colour'];
+			}
+		}
+		
+		// If there's no title, get some text from content to be one
+		if(empty($title)) {
+			$words = explode(' ', $reply->comment_content);
+			$title = implode(' ', array_slice($words, 0, 3));
+			$title = substr($title, 0, 70);
+			
+			// Remove these "special" characters from the end of string
+			if(in_array(substr($title, -1), array( ',', '.', ' ', '!', '?' )))
+				$title = substr($title, 0, -1);
+				
+			$title .= '...';
+		}
+		
+		// Check if there's no index set for the note
+		if(empty($node_index)) {
+			$node_index = $map_index;
+			$map_index++;
+			update_comment_meta( $reply->comment_ID, 'knbu_map_comment_index', $node_index );
+			update_post_meta( $id, 'knbu_map_index', $map_index);
+		}
+		
+		$nodes[] = array(
+				'id' => $reply->comment_ID,
+				'parent' => $reply->comment_parent,
+				'content' => $reply->comment_content,
+				'avatar' => knbu_get_avatar_url($reply->user_id),
+				'username' => $reply->comment_author,
+				'email' => $reply->user_email,
+				'date' => date(get_option('date_format').' '.get_option('time_format'), strtotime($reply->comment_date)),
+				'timestamp' => strtotime($reply->comment_date),
+				'title' => $title,
+				'typeName' => $name,
+				'anchor' => $anchor,
+				'color' => $color,
+				'index' => $node_index
+		);
+	}
+	
+	echo '<script type="text/javascript">var NodesFromServer = '.json_encode($nodes).'; var MapIndexFromServer = '.$map_index.'; var plugin_url = "'.plugins_url('knowledge-building').'";</script>';
+}
+
+
+function knbu_cmp($a, $b) {
+	if(strtotime($a->comment_date) == strtotime($b->comment_date)) 
+		return 0;
+	return strtotime($a->comment_date) > strtotime($b->comment_date) ? 1 : -1;
+}
+?>
